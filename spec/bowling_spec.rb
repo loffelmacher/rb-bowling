126dccce9837e501d62bf09b8d1ed41974716afc
Randy L <randy.loffelmacher@vroom.com>
2018-07-01 11:35:55 -0400

    second test passes

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,15 +2,27 @@ require './lib/game'
 
 game = Game.new
 
-describe 'game' do
-	it 'returns 0 if all gutter balls' do
-		for ii in (0...21)
-			game.roll(0)	
+module Helpers
+	def rollMany(game, times, score)
+		for ii in (0...times)
+			game.roll(score)
 		end
-		expect(game.score()).to eq 0
 	end
 end
 
-#begin _rollMany()
-	
-#end
+RSpec.configure do |c|
+  c.include Helpers
+end
+
+describe 'a game of bowling' do
+	it 'scores 0 if all gutter balls' do
+		rollMany(game, 20, 0)
+		expect(game.score()).to eq 0
+	end
+
+	it 'scores 20 if all ones' do 
+		rollMany(game, 20, 1)
+		expect(game.score()).to eq 20
+	end
+end
+
