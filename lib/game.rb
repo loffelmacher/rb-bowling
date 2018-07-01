80169e837eec510cc93e0fcbe12a9e3978f68a3a
Randy L <randy.loffelmacher@vroom.com>
2018-07-01 12:40:11 -0400

    passes third test

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,40 @@
 class Game
   def initialize
     @rolls = Array.new(21, 0)
+    @currentRoll = 0
   end
 
   def roll(pins)
-  	@rolls.push(pins)
+    @rolls[@currentRoll] = pins
+    @currentRoll += 1
   end
 
 	def score()
-		score = 0
-		@rolls.each do |roll|
-			score = score + roll
-		end
+		score = 0 
+    frameIndex = 0
+    for frame in (0...10)
+      if isSpare?(frameIndex)
+        score += 10 + spareBonus(frameIndex)
+        frameIndex += 2
+      else
+        score += sumOfBallsInFrame(frameIndex)
+        frameIndex += 2
+      end
+    end
 		score
+  end
+
+  private
+
+  def isSpare?(frameIndex)
+    @rolls[frameIndex] + @rolls[frameIndex+1] == 10
+  end
+
+  def spareBonus(frameIndex)
+    @rolls[frameIndex+2]
+  end
+
+  def sumOfBallsInFrame(frameIndex) 
+    @rolls[frameIndex] + @rolls[frameIndex+1]
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,12 +1,15 @@
 require './lib/game' 
 
-game = Game.new
-
 module Helpers
-	def rollMany(game, times, score)
+	def rollMany(game, times, pins)
 		for ii in (0...times)
-			game.roll(score)
+			game.roll(pins)
 		end
+	end
+
+	def rollSpare(game)
+		game.roll(5)
+		game.roll(5)
 	end
 end
 
@@ -14,22 +17,26 @@ RSpec.configure do |c|
   c.include Helpers
 end
 
-describe 'a game of bowling' do
+describe Game do
+	before :each do
+	  @game = Game.new
+	end
+
 	it 'scores 0 if all gutter balls' do
-		rollMany(game, 20, 0)
-		expect(game.score()).to eq 0
+		rollMany(@game, 20, 0)
+		expect(@game.score()).to eq 0
 	end
 
 	it 'scores 20 if all ones' do 
-		rollMany(game, 20, 1)
-		expect(game.score()).to eq 20
+		rollMany(@game, 20, 1)
+		expect(@game.score()).to eq 20
 	end
 
 	it 'scores 16 if one spare' do 
-		game.roll(5)
-		game.roll(5)
-		game.roll(3)
-		expect(game.score()).to eq 16
+		rollSpare(@game)
+		@game.roll(3)
+		rollMany(@game, 17, 0)
+		expect(@game.score()).to eq 16
 	end
 end
 
